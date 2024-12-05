1b4e5b6cf5bf39c70d0efa169444215a8d67a1c9
Roman <s-3m@yandex.ru>
2024-12-05 19:26:47 +0300

    format float string

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from dotenv import load_dotenv
 
 BASE_URL = "https://contract.mexc.com"
 
-semaphore = asyncio.Semaphore(650)
-unique_pair = set()
-load_dotenv(".env")
-bot_token = os.getenv("BOT_TOKEN")
-user_id = os.getenv("USER_ID")
-print(bot_token)
-print(user_id)
+# semaphore = asyncio.Semaphore(650)
+# unique_pair = set()
+# load_dotenv(".env")
+# bot_token = os.getenv("BOT_TOKEN")
+# user_id = os.getenv("USER_ID")
+# print(bot_token)
+# print(user_id)
 
 
 async def bot_notify(data: dict):
@@ -46,8 +46,8 @@ async def check_to_pump(session, pair):
                 if percent > 5:
                     pump_params = {
                         "currency": pair,
-                        "open": open,
-                        "close": close,
+                        "open": f"{open:.20f}".rstrip("0").rstrip("."),
+                        "close": f"{close:.20f}".rstrip("0").rstrip("."),
                         "percent": round(percent, 2),
                     }
                     await bot_notify(pump_params)
@@ -71,4 +71,7 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    # asyncio.run(main())
+
+    a = 0.000002452
+    print(f"{a:.20f}".rstrip("0").rstrip("."))
